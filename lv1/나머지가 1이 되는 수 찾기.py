199a1f901e774f0b3c5cb01656899f5e0d7a1c99
ykgod7 <ejihwan7@gmail.com>
2022-07-03 23:24:56 +0900

    ??

--- lv1/나머지가 1이 되는 수 찾기.py
+++ lv1/나머지가 1이 되는 수 찾기.py
@@ -1,4 +1,22 @@
-def solution(n):
-    for i in range(2, n):
-        if n % i == 1:
-            return i
+def solution(s):
+    answer = []
+    if len(s) == 1:
+        return len(s)
+    for i in range(1, int(len(s)/2)+1):
+        count, temp, word = 0, s[:i], ''
+        for a  in [s[j:j+i] for j in range(0, len(s), i)]:
+            if temp == a:
+                count += 1
+            else:
+                if count > 1:
+                    word += str(count) + temp
+                else:
+                    word += temp
+                temp = a
+                count = 1
+        if count > 1:
+            word += str(count) + temp
+        else:
+            word += temp
+        answer.append(len(word))
+    return min(answer)
