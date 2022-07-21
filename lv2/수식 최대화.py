de32ee44102f4a29b8e0ebfa5d8723559a1a8122
ykgod7 <ejihwan7@gmail.com>
2022-07-21 21:52:44 +0900

    7/21

--- lv2/수식 최대화.py
+++ lv2/수식 최대화.py
@@ -1,32 +1,13 @@
-from itertools import permutations
+import re
 
-def solution(expression):
-    combs = list(permutations(['*', '+', '-'], 3))
+def solution(s):
+    answer = []
+    st = re.sub('},{', '}.{', s)
+    st = st.split('.')
 
-    num, ex = '', []
-    for _ in expression:
-        if _.isdigit():
-            num += _
-        else:
-            ex.append(num)
-            ex.append(_)
-            num = ''
-    ex.append(num)
-    temp = ex[:]
-    largest = 0
-    for comb in combs:
-        ex = temp[:]
-        for _ in comb:
-            for i in range(ex.count(_)):
-                idx = ex.index(_)
-                if _ == '*':
-                    ex[idx-1] = int(ex[idx-1]) * int(ex[idx+1])
-                elif _ == '+':
-                    ex[idx-1] = int(ex[idx-1]) + int(ex[idx+1])
-                else:
-                    ex[idx-1] = int(ex[idx-1]) - int(ex[idx+1])
-                ex.pop(idx)
-                ex.pop(idx)
-        if abs(ex[0]) > largest:
-            largest = abs(ex[0])
-    return largest
+    for _ in sorted(st, key=len):
+        for num in re.findall('\d+', _):
+            if int(num) not in answer:
+                answer.append(int(num))
+
+    return answer
---
+++ lv2/튜플_수정.py
@@ -0,0 +1,6 @@
+import re
+from collections import Counter
+
+def solution(s):
+    st = Counter(re.findall('\d+', s))
+    return list(map(int, [k for k, v in sorted(st.items(), key=lambda x: x[1], reverse=True)]))
